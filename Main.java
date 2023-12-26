0296d15ae398327e7788f2428ba4f6ddb32ed1f9
Allan Branson <bransonaallan@gmail.com>
2023-12-26 06:40:11 +0300

    Breadth First Search

--- Main.java
+++ Main.java
@@ -5,6 +5,15 @@ public class Main {
     //                      or already visited a node,
     //                      Backtrack to a previous node with unvisited adjascent node
 
+/*
+ * Breadth FS VS Depth FS Comparison
+ * Breadth FS traverses a graph by level while Depth FS traverses a graph branch by branch
+ * Breadth FS utilizes a queue while a Depth FS utilizes a stack
+ * Breadth FS is better if the destination is close to start while a Depth fs is better if destination is on average far from the start
+ * Breadth FS siblisngs are visited befor children while in Depth FS children are visited befor siblings
+ * Depth FS is more popular for games and puzzles
+ */
+
 
 
     public static void main(String []Args) {
@@ -28,11 +37,17 @@ public class Main {
         graph.print();
 
         //Check if a node has an edge
-        System.out.println("");
-        System.out.println("Does an edge exist? " + graph.checkEdge(+2,1));
-        System.out.println("");
+        //System.out.println("\n");
+        //System.out.println("Does an edge exist? " + graph.checkEdge(+2,1));
+        //System.out.println("\n");
 
-        //Visit a node 
-        graph.depthFirstSearch(4);
+
+        //Visit a node using depthfirst seach
+        //graph.depthFirstSearch(0);
+
+
+        //Visit a node
+        System.out.println("\n"); 
+        graph.breadthFirstSearch(3);
     }
 }
